c313efe3f11b935afd7a07d0f9e7f6678450c621
evan holt <adiga_knight@hotmail.com>
2020-12-09 15:54:11 +0200

    cors error bugfix

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const path = require('path')
+var cors = require('cors')
 
 const app = express();
+
+app.use(cors())
 
 app.get('/hurricane', (req, res) => {
     res.download(__dirname + '/hurricanes.csv');
