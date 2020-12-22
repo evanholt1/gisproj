3fded83bb8d15572a46190283583dce8298b09f7
evan holt <adiga_knight@hotmail.com>
2020-12-22 17:50:30 +0200

    added cars csv

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const express = require('express')
-const path = require('path')
-var cors = require('cors')
+const express = require("express");
+const path = require("path");
+var cors = require("cors");
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.get('/hurricane', (req, res) => {
-    res.download(__dirname + '/hurricanes.csv');
-})
+app.get("/hurricane", (req, res) => {
+    res.download(__dirname + "/hurricanes.csv");
+});
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/view.html")
-})
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/view.html");
+});
 
-app.get('/cities', (req, res) => {
-    res.download(__dirname + '/US-Largest-1000-Cities.csv');
-})
+app.get("/cities", (req, res) => {
+    res.download(__dirname + "/US-Largest-1000-Cities.csv");
+});
 
-app.use(express.static(path.join(__dirname, "public")))
+app.get("/cars", (req, res) => {
+    res.download(__dirname + "/cars.csv");
+});
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(process.env.PORT || 5000);
